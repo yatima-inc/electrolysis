70b9598686c76b318632d67439ee6cdb7f7c335e
Sebastian Ullrich <sebasti@nullri.ch>
2015-10-10 12:45:39 +0200

    More Applicative lifting

--- Rustabelle.thy
+++ Rustabelle.thy
@@ -7,10 +7,11 @@ type_synonym ('state, 'res, 'ans) sem = "('state, 'res, 'ans) cont \<Rightarrow>
 
 type_synonym u32 = nat
 
-abbreviation seq :: "('state, unit, 'ans) sem \<Rightarrow> ('state, 'res, 'ans) sem \<Rightarrow> ('state, 'res, 'ans) sem" (infix ";" 20) where
-  "s1; s2 \<equiv> \<lambda>c. s1 (\<lambda>s _. s2 c s)"
+abbreviation lift :: "('r1 \<Rightarrow> 'r2) \<Rightarrow> ('state, 'r1, 'ans) sem \<Rightarrow> ('state, 'r2, 'ans) sem" where
+  "lift f s \<equiv> \<lambda>c. s (\<lambda>s r. c s (f r))"
 
-abbreviation lift2 :: "('res \<Rightarrow> 'res \<Rightarrow> 'res) \<Rightarrow> ('state, 'res, 'ans) sem \<Rightarrow> ('state, 'res, 'ans) sem \<Rightarrow> ('state, 'res, 'ans) sem" where
+abbreviation lift2 :: "('r1 \<Rightarrow> 'r2 \<Rightarrow> 'r3) \<Rightarrow> ('state, 'r1, 'ans) sem \<Rightarrow> ('state, 'r2, 'ans) sem \<Rightarrow> ('state, 'r3, 'ans) sem" where
   "lift2 f s1 s2 \<equiv> \<lambda>c. s1 (\<lambda>s r\<^sub>1. s2 (\<lambda>s r\<^sub>2. c s (f r\<^sub>1 r\<^sub>2)) s)"
 
+abbreviation "seq s1 s2 \<equiv> lift2 (\<lambda>_ r2. r2) s1 s2"
 end
