18b9ca04eceb6805a7dd0b61c7317727966eb474
Sebastian Ullrich <sebasti@nullri.ch>
2016-03-21 15:41:55 +0100

    Prove a first lemma about https://github.com/Kha/leantc-rs.git

--- thys/alloc_export.thy
+++ thys/alloc_export.thy
@@ -8,4 +8,9 @@ datatype 'T alloc_rc_Rc =
 datatype 'T alloc_raw_vec_RawVec =
   alloc_raw_vec_RawVec "'T list"
 
+definition [simp]: "alloc_rc_Rc_T__new x = Some (alloc_rc_Rc x)"
+
+definition [simp]: "alloc_rc_Rc_T__Deref_deref rc \<equiv> case rc of
+  alloc_rc_Rc x \<Rightarrow> Some x"
+
 end
