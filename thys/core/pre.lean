68b8c63bdf58cb6f93bb37b42735714d73766e8b
Sebastian Ullrich <sebasti@nullri.ch>
2016-04-14 14:55:39 +0200

    Some loop_eq

--- thys/core/pre.lean
+++ thys/core/pre.lean
@@ -43,6 +43,8 @@ namespace option
   end
 end option
 
+open option
+
 notation `do` binder ` ← ` x `; ` r:(scoped f, option.bind f x) := r
 
 open [class] classical
@@ -59,27 +61,72 @@ begin
   { apply classical.some_spec }
 end
 
+theorem dite_else_false {H : Prop} {t : H → Prop} (Hdite : if c : H then t c else false) : H :=
+begin
+  apply dite H,
+  { apply id },
+  { intro Hneg,
+    rewrite (dif_neg Hneg) at Hdite,
+    apply false.elim Hdite, }
+end
+
 section
-  parameters {A B : Type}
-  parameters (F : (A → option B) → A → option B)
-  variable (R : A → A → Prop)
+  parameters {State : Type}
 
-  noncomputable definition fix_opt.F (x : A) (f : Π (y : A), R y x → option B) : option B :=
-  F (λy, if H : R y x then f y H else none) x
+  inductive loop.control := continue | break
 
-  noncomputable definition fix_opt.fix (Hwf: well_founded R) (x : A) : option B :=
-  @well_founded.fix A (λx, option B) R Hwf (fix_opt.F R) x
+  parameters (body : State → State × loop.control)
+  variable (R : State → State → Prop)
 
-  noncomputable definition fix_opt.wf_R :=
-  if Hwf : well_founded R then ∀x : A, fix_opt.fix R Hwf x ≠ none else false
+  noncomputable definition loop.F (s : State) (f : Π (s' : State), R s' s → option State) : option State :=
+  match body s with
+  | (s', continue) := if H : R s' s then f s' H else none
+  | (s', break)    := some s'
+  end
 
-  noncomputable definition fix_opt (x : A) : option B :=
-  do R ← some_opt fix_opt.wf_R;
-  if Hwf : well_founded R then fix_opt.fix R Hwf x else none
+  noncomputable definition loop.fix (Hwf: well_founded R) (s : State) : option State :=
+  well_founded.fix (loop.F R) s
 
-  theorem fix_opt_eq {R : A → A → Prop} [Hwf : well_founded R] (Hf_wf : ∀x : A, fix_opt.fix R Hwf x ≠ none) (x : A) :
-    fix_opt x = F (λx, fix_opt x) x :=
-  sorry
+  noncomputable definition loop.wf_R :=
+  if Hwf : well_founded R then ∀s : State, loop.fix R Hwf s ≠ none else false
+
+  noncomputable definition loop (s : State) : option State :=
+  do R ← some_opt loop.wf_R;
+  if Hwf : well_founded R then loop.fix R Hwf s else none
+
+  /-theorem loop_inv
+    {A : Type}
+    (P Q : A → Prop) {R : A → A → Prop}
+    {s : A} {l : (A → option A) → A → option A}
+    (Hstart : P s)
+    (Hdoes_step : ∀s f, P s → ∃s', l f s = some s' ∨ l f s = f s')
+    (Hstep : ∀f s s', P s → l f s = f s' → P s')
+    (Hstop : ∀f s s', P s → l f s = some s' → Q s')
+    (Hwf_R : well_founded R)
+    (HR : ∀f s s', P s → l f s = some s' → R s' s) :
+    option.all Q (fix_opt l s) :=-/
+
+  theorem loop_eq
+    [Hwf_R : well_founded R]
+    (HR : ∀s s', body s = (s', loop.control.continue) → R s' s)
+    (s : State) :
+    loop s = match body s with
+    | (s', continue) := loop s'
+    | (s', break)    := some s'
+    end :=
+  have ∀(s : State), loop.fix R Hwf_R s ≠ none, from sorry,
+  obtain R' (HR' : some_opt loop.wf_R = some R') (Hloop_wf_R' : loop.wf_R R'),
+  from some_opt.ex R (decidable.rec_on_true _ this),
+  begin
+    clear H_obtain_from,
+    note Hwf_R' := dite_else_false Hloop_wf_R',
+    rewrite [↑loop, HR', ▸*, dif_pos Hwf_R'],
+    rewrite [↑loop.fix, well_founded.fix_eq, ↑loop.F],
+    apply (congr_arg (prod.cases_on (body s))),
+    apply funext, intro s',
+    have R' s' s, from sorry,
+    rewrite [dif_pos this, dif_pos Hwf_R'],
+  end
 end
 
 abbreviation u8 := nat
