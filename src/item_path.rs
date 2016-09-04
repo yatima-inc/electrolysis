b5716112b4952e97efabd0132cb149e3d7bcbe6e
Sebastian Ullrich <sebasti@nullri.ch>
2016-09-03 21:21:41 -0400

    Fix item paths

--- src/item_path.rs
+++ src/item_path.rs
@@ -1,6 +1,8 @@
 use rustc::hir::map::definitions::DefPathData;
 use rustc::hir::def_id::DefId;
-use rustc::ty::TyCtxt;
+use rustc::ty::{self, TyCtxt};
+
+use itertools::Itertools;
 
 // from ../rust/src/librustc/ty/item_path.rs, but simpler impl presentation
 
@@ -40,16 +42,34 @@ pub fn push_item_path(tcx: TyCtxt, buffer: &mut Vec<String>, def_id: DefId)
 
 fn push_impl_path(tcx: TyCtxt, buffer: &mut Vec<String>, impl_def_id: DefId)
 {
-    // we suppress the unnecessary parent path, but still need the crate root
-    if !impl_def_id.is_local() {
-        buffer.push(tcx.sess.cstore.crate_name(impl_def_id.krate).to_string());
-    }
+    let self_ty = tcx.lookup_item_type(impl_def_id).ty;
     if let Some(trait_ref) = tcx.impl_trait_ref(impl_def_id) {
-        buffer.push(format!("{} as {}",
-                            tcx.lookup_item_type(impl_def_id).ty,
-                            trait_ref))
+        // we suppress the unnecessary parent path, but still need the crate root
+        if !impl_def_id.is_local() {
+            buffer.push(tcx.sess.cstore.crate_name(impl_def_id.krate).to_string());
+        }
+        buffer.push(format!("{} as {}", self_ty, trait_ref))
     } else {
-        buffer.push(format!("impl {}",
-                            tcx.lookup_item_type(impl_def_id).ty))
+        let name = format!("{}", self_ty);
+        match self_ty.sty {
+            ty::TyStruct(..) | ty::TyEnum(..) => {
+                // `local_crate::a::B<c::D>` ~> `a.«B<c::D>»`
+                let sep = name.find("<").unwrap_or(name.len());
+                let mut parts = name[..sep].split("::").map(ToString::to_string).collect_vec();
+                if self_ty.ty_to_def_id().unwrap().is_local() {
+                    // remove crate root
+                    parts = parts.into_iter().skip(1).collect();
+                }
+                *parts.last_mut().unwrap() += &name[sep..];
+                buffer.extend(parts);
+            }
+            _ => {
+                // `[T]` ~> `collections.«[T]»`
+                if !impl_def_id.is_local() {
+                    buffer.push(tcx.sess.cstore.crate_name(impl_def_id.krate).to_string());
+                }
+                buffer.push(name)
+            }
+        }
     }
 }
